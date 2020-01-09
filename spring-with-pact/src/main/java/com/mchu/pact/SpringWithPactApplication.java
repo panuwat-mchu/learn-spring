fe2ff246154f8693f4f9357d84f94e5dbf6ba086
panuwat <panuwat.codes@gmail.com>
2020-01-10 01:01:36 +0700

    Initiate

---
+++ spring-with-pact/src/main/java/com/mchu/pact/SpringWithPactApplication.java
@@ -0,0 +1,13 @@
+package com.mchu.pact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithPactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithPactApplication.class, args);
+	}
+
+}
---
+++ spring-with-pact/src/test/java/com/mchu/pact/SpringWithPactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchu.pact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithPactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
