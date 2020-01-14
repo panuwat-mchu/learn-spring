ae6370247107552e36c45b79c39c26c8e61e0fb5
panuwat <panuwat.codes@gmail.com>
2020-01-14 23:44:17 +0700

    add pact provider test

---
+++ spring-with-pact-provider/src/main/java/com/mchu/SpringWithPactProviderApplication.java
@@ -0,0 +1,13 @@
+package com.mchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithPactProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithPactProviderApplication.class, args);
+	}
+
+}
---
+++ spring-with-pact-provider/src/test/java/com/mchu/SpringWithPactProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithPactProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
