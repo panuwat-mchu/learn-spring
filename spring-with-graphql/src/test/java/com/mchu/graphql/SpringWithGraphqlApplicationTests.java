ccd8ff833096042c6861677bcb3a945cc251894f
panuwat <panuwat.codes@gmail.com>
2020-08-06 23:35:02 +0700

    Initiate.

---
+++ spring-with-graphql/src/test/java/com/mchu/graphql/SpringWithGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchu.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
