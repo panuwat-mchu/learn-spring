41eceae92123cde21c2af7f4c0012258e32a8d33
panuwat <panuwat.codes@gmail.com>
2020-08-16 23:37:24 +0700

    config security.

---
+++ spring-boot-with-graphql-jpa/src/main/java/com/mchu/bookstore/SpringBootWithGraphqlJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mchu.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGraphqlJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithGraphqlJpaApplication.class, args);
+	}
+
+}
