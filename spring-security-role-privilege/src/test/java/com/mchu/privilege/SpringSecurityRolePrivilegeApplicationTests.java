52cc7ba5efd2786f9185c91f0e3772f784029ff7
panuwat <panuwat.codes@gmail.com>
2020-08-18 20:03:30 +0700

    initiate.

---
+++ spring-security-role-privilege/src/test/java/com/mchu/privilege/SpringSecurityRolePrivilegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchu.privilege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRolePrivilegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
