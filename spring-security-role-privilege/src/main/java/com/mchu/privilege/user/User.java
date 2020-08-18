a808b24f82055c55bf934f8111cb8eeb76e83d86
panuwat <panuwat.codes@gmail.com>
2020-08-18 20:06:43 +0700

    initiate.

---
+++ spring-security-role-privilege/src/main/java/com/mchu/privilege/user/User.java
@@ -0,0 +1,37 @@
+package com.mchu.privilege.user;
+
+import com.mchu.privilege.role.Role;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean enabled;
+    private boolean tokenExpired;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"
+            )
+    )
+    private Collection<Role> roles;
+
+}
---
+++ spring-security-role-privilege/src/main/java/com/mchu/privilege/user/UserController.java
@@ -0,0 +1,24 @@
+package com.mchu.privilege.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@Slf4j
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
+    public String getAllUsers(@AuthenticationPrincipal Principal auth){
+        if(auth != null)
+            log.debug(auth.toString());
+        log.debug(">hello");
+        return "Hello";
+    }
+}
