579853b03dcbc6389b6254713b50dd20cd3ded5f
panuwat <panuwat.codes@gmail.com>
2020-12-17 18:12:31 +0700

    learn tdd.

---
+++ with-tdd/src/main/java/mchu/learn/spring/WithTddApplication.java
@@ -0,0 +1,13 @@
+package mchu.learn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithTddApplication.class, args);
+	}
+
+}
